0a9cc0db4d86aa0583bfbdcb2f142b68d1cdc363
Uday Kiran <161811840+udaykirandatascience@users.noreply.github.com>
2024-02-29 23:39:14 -0800

    Add files via upload

---
+++ api_implementation.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 29 22:16:32 2024
+
+@author: QITS
+"""
+
+import json
+import requests
+
+
+url = 'http://127.0.0.1:8000/diabetes_prediction'
+
+
+input_data_for_model = {
+    
+    'Pregnancies' : 6,
+    'Glucose' : 148,
+    'BloodPressure' : 72,
+    'SkinThickness' : 35,
+    'Insulin' : 0,
+    'BMI' : 33.6,
+    'DiabetesPedigreeFunction' : 0.627,
+    'Age' : 50
+    
+    }
+
+
+input_json = json.dumps(input_data_for_model)
+
+response = requests.post(url, data=input_json)
+
+print(response.text)
